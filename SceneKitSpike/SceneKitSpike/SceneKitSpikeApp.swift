f0844a0641f85481fcabfffae10c93143e37c0fc
Andre Pham <andrepham33@gmail.com>
2023-03-05 17:42:26 +1100

    App init

---
+++ SceneKitSpike/SceneKitSpike/SceneKitSpikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneKitSpikeApp.swift
+//  SceneKitSpike
+//
+//  Created by Andre Pham on 5/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct SceneKitSpikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
